da7e38054bd976dcc6e2bda3184dbe877c305911
root <root@mail.etsb.com>
2016-01-14 16:13:16 +0600

    admin

---
+++ app/modules/admin/routes.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: etsb
+ * Date: 1/14/16
+ * Time: 3:59 PM
+ */
+
+Route::group(array('modules'=>'Admin', 'namespace' => 'App\Modules\Admin\Controllers'), function() {
+    //Your routes belong to this module.
+
+
+    Route::any('admin', [
+        'as' => 'admin',
+        'uses' => 'AdminController@index'
+    ]);
+
+    Route::get('/', function () {
+        return 'Hello World';
+    });
+
+
+});
+
