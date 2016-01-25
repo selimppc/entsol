99dbeca104469215bc63a8fe81795699093d0ae8
Tanin <tanin@edutechsolutionsbd.com>
2016-01-25 14:42:27 +0600

    voucher form

---
+++ app/modules/accounts/Models/Balance.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: etsb
+ * Date: 1/25/16
+ * Time: 2:35 PM
+ */
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Balance extends Model
+{
+
+}
---
+++ app/modules/accounts/Views/voucher_head/_form.blade.php
@@ -0,0 +1,89 @@
+@if($errors->any())
+    <ul class="alert alert-danger">
+        @foreach($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
+
+
+<div class="form-group no-margin-hr panel-padding-h no-padding-t no-border-t">
+    <div class="row">
+        {!! Form::label('type', 'Type:', ['class' => 'control-label']) !!}
+        <small class="required">(Required)</small>
+        {!! Form::Select('type',array('account-payable'=>'Account Payable','account-receivable'=>'Account Receivable','account-adjustment'=>'account Adjustment','journal-vouche'=>'Journal Voucher','receipt-voucher'=>'Receipt Voucher','reverse-entry'=>'Reverse Entry'),Input::old('type'),['class'=>'form-control ','required']) !!}
+    </div>
+</div>
+
+<div class="form-group no-margin-hr panel-padding-h no-padding-t no-border-t">
+    <div class="row">
+        {!! Form::label('date', 'Date:', ['class' => 'control-label']) !!}
+        <div class="input-group date" id="bs-datepicker-component">
+            {!! Form::text('date', null, ['class' => 'form-control','required']) !!}
+            <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+        </div>
+    </div>
+</div>
+
+<div class="form-group no-margin-hr panel-padding-h no-padding-t no-border-t">
+    <div class="row">
+        {!! Form::label('reference', 'Reference:', ['class' => 'control-label']) !!}
+        {!! Form::text('reference', null, ['class' => 'form-control','required']) !!}
+    </div>
+</div>
+
+<div class="form-group no-margin-hr panel-padding-h no-padding-t no-border-t">
+    <div class="row">
+        {!! Form::label('year', 'Year:', ['class' => 'control-label']) !!}
+        {!! Form::text('year', null, ['class' => 'form-control','required']) !!}
+    </div>
+</div>
+
+<div class="form-group no-margin-hr panel-padding-h no-padding-t no-border-t">
+    <div class="row">
+        {!! Form::label('period', 'Period:', ['class' => 'control-label']) !!}
+        {!! Form::text('period', null, ['class' => 'form-control','required']) !!}
+    </div>
+</div>
+
+<div class="form-group no-margin-hr panel-padding-h no-padding-t no-border-t">
+    <div class="row">
+        {!! Form::label('branch_id', 'Branch:', ['class' => 'control-label']) !!}
+        {!! Form::text('branch_id', null, ['class' => 'form-control','required']) !!}
+    </div>
+</div>
+
+<p> &nbsp; </p>
+
+<div class="modal-footer">
+    <a href="{{route('voucher-head')}}"  class="btn btn-default" type="button"> Close </a>
+    {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
+</div>
+
+<!-- Javascript -->
+<script>
+    init.push(function () {
+        var options = {
+            todayBtn: "linked",
+            orientation: $('body').hasClass('right-to-left') ? "auto right" : 'auto auto'
+        }
+        $('#bs-datepicker-example').datepicker(options);
+
+        $('#bs-datepicker-component').datepicker();
+
+        var options2 = {
+            orientation: $('body').hasClass('right-to-left') ? "auto right" : 'auto auto'
+        }
+        $('#bs-datepicker-range').datepicker(options2);
+
+        $('#bs-datepicker-inline').datepicker();
+    });
+</script>
+
+<!-- / Javascript -->
+<script type="text/javascript">
+    init.push(function () {
+        // Javascript code here
+    })
+    window.LanderApp.start(init);
+</script>
