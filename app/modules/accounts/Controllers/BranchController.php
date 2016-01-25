2d1a78e3d5e6438295929e623afbe973e071edb3
Shajjad Hossain <shajjad@edutechsolutionsbd.com>
2016-01-25 15:56:59 +0600

    migration+branch page

--- app/modules/accounts/Controllers/BranchController.php
+++ app/modules/accounts/Controllers/BranchController.php
@@ -56,4 +56,78 @@ class BranchController extends Controller
 
         return redirect()->back();
     }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {//print_r($id);exit;
+        $pageTitle = 'Show the detail';
+        $data = Branch::where('id',$id)->first();
+
+        return view('accounts::branch.view', ['data' => $data, 'pageTitle'=> $pageTitle]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $pageTitle = 'Show the detail';
+        $data = Branch::where('id',$id)->first();
+        $currency_id = Currency::lists('title','id');
+        return view('accounts::branch.update', ['data' => $data, 'pageTitle'=> $pageTitle, 'currency_id'=> $currency_id]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Requests\BranchRequest $request, $id)
+    {
+        $model = Branch::where('id',$id)->first();
+        $input = $request->all();
+
+        DB::beginTransaction();
+        try {
+            $model->update($input);
+            DB::commit();
+            Session::flash('flash_message', "Successfully Updated");
+        }
+        catch ( Exception $e ){
+            //If there are any exceptions, rollback the transaction
+            DB::rollback();
+            Session::flash('flash_message_error', $e->getMessage());
+        }
+        return redirect()->back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function delete($id)
+    {
+        try {
+            $model = Branch::where('id',$id)->first();
+            if ($model->delete()) {
+                Session::flash('flash_message', " Successfully Deleted.");
+                return redirect()->back();
+            }
+        } catch(\Exception $e) {
+            Session::flash('flash_message_error',$e->getMessage() );
+            return redirect()->back();
+        }
+    }
 }
---
+++ app/modules/accounts/Views/branch/update.blade.php
@@ -0,0 +1,10 @@
+<div class="modal-header">
+    <a href="{{ URL::previous() }}" class="close" type="button"> × </a>
+    <h4 class="modal-title" id="myModalLabel">{{$pageTitle}}</h4>
+</div>
+
+<div class="modal-body">
+    {!! Form::model($data, ['method' => 'PATCH', 'route'=> ['branch-update', $data->id]]) !!}
+    @include('accounts::branch._form')
+    {!! Form::close() !!}
+</div>
