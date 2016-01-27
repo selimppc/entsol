f983213c76a0d5ed81d03cff7862c63d2c4fc89f
Shajjad Hossain <shajjad@edutechsolutionsbd.com>
2016-01-27 15:24:49 +0600

    migration page modified

--- app/modules/accounts/database/migrations/2016_01_21_112944_create_entsol_table_accounts.php
+++ app/modules/accounts/database/migrations/2016_01_21_112944_create_entsol_table_accounts.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateEntsolTableTable extends Migration
+class CreateEntsolTableAccounts extends Migration
 {
     /**
      * Run the migrations.
