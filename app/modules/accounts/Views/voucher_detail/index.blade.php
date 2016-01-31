b628444d9276f4cffbe288657c691c264c8fd744
Tanin <tanin@edutechsolutionsbd.com>
2016-01-31 10:11:20 +0600

    #1 voucher detail back button

--- app/modules/accounts/Views/voucher_detail/index.blade.php
+++ app/modules/accounts/Views/voucher_detail/index.blade.php
@@ -81,8 +81,10 @@
 
                         </tbody>
                     </table>
+
                 </div>
                     {{--<span class="pull-left">{!! str_replace('/?', '?', $model->render()) !!} </span>--}}
+                <a class="pull-right btn btn-xs btn-primary" href="{{ URL::route('voucher-head')}}"> <i class="fa fa-arrow-circle-left"></i> Back To Journal Voucher</a>
             </div>
         </div>
     </div>
