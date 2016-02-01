3eaf0d4376377991a18da58e63d190d8422f19c6
Selim Reza <selim@ETSB.com>
2016-02-01 12:15:39 +0600

    Issue #1

--- app/modules/accounts/database/migrations/2016_02_01_054114_create_store_procedure_for_ac.php
+++ app/modules/accounts/database/migrations/2016_02_01_054114_create_store_procedure_for_ac.php
@@ -13,7 +13,21 @@ class CreateStoreProcedureForAc extends Migration
      */
     public function up()
     {
-        DB::unprepared('CREATE PROCEDURE test() BEGIN SELECT * FROM user; END');
+        DB::unprepared('
+        CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_voucher_post`(
+        v_voucher_number VARCHAR(50),
+        p_user VARCHAR(50)
+        )
+        BEGIN
+            INSERT INTO balance(voucher_head_id, voucher_number,coa_id, account_code,sub_account_code,DATE,branch_id,reference,YEAR,period,currency_id,exchange_rate,prime_amount,base_amount,STATUS,created_at,created_by)
+            SELECT a.id, a.voucher_number,b.coa_id, b.account_code,b.sub_account_code,a.date,a.branch_id,a.reference,a.year,a.period,b.currency_id,b.exchange_rate,b.prime_amount,b.base_amount,\'post\',CURRENT_TIMESTAMP,p_user
+            FROM `entsol`.`ac_voucher_head` a
+            INNER JOIN `entsol`.`ac_voucher_detail` b ON a.id=b.voucher_head_id
+            WHERE a.status=\'balanced\' AND a.voucher_number=v_voucher_number;
+
+            UPDATE `entsol`.`ac_voucher_head` SET STATUS=\'posted\' WHERE voucher_number=v_voucher_number AND STATUS=\'balanced\';
+        END;
+        ');
     }
 
     /**
@@ -23,7 +37,7 @@ class CreateStoreProcedureForAc extends Migration
      */
     public function down()
     {
-        $sql = "DROP PROCEDURE IF EXISTS spGetReturnables";
+        $sql = "DROP PROCEDURE IF EXISTS sp_voucher_post";
         DB::connection()->getPdo()->exec($sql);
     }
 }
