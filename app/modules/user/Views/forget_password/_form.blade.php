abc7399e653f2e803e7873b153faaf6577e09f23
Nadim <nhsajib316@gmail.com>
2016-02-08 11:51:41 +0600

    User login with username or email

---
+++ app/modules/user/Views/forget_password/_form.blade.php
@@ -0,0 +1,25 @@
+@extends('user::layouts.signup')
+
+@section('content')
+
+        <!-- Form -->
+<div class="signup-form">
+
+    {!! Form::open(['route' => 'forget-password','id'=>'signup-form_id']) !!}
+
+        <div class="signup-text">
+            <span>Enter Your Email</span>
+        </div>
+
+        <div class="form-group">
+            {!! Form::email('email', null, ['class' => 'form-control input-lg','required','placeholder'=>'E-mail']) !!}
+        </div>
+
+        <div class="form-actions">
+            <input type="submit" value="Reset Password" class="signup-btn bg-primary">
+        </div>
+    {!! Form::close() !!}
+    <!-- / Form -->
+</div>
+
+@stop
