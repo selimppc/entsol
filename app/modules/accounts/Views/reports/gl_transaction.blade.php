a0eafdd679a11aaa91d3441525903ad1e0aeb397
shajjadhossain <shajjadhossain81@gmail.com>
2016-02-10 16:42:57 +0600

    #1 GL Transation report modal complete

--- app/modules/accounts/Views/reports/gl_transaction.blade.php
+++ app/modules/accounts/Views/reports/gl_transaction.blade.php
@@ -1,7 +1,40 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: etsb
- * Date: 2/10/16
- * Time: 4:37 PM
- */
+<div class="form-group no-margin-hr panel-padding-h no-padding-t no-border-t">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="col-sm-3">
+                {!! Form::label('pTrn', 'PTrn:', ['class' => 'control-label']) !!}
+                {!! Form::text('pTrn', null, ['id'=>'pTrn', 'class' => 'form-control','required','title'=>'enter pTrn']) !!}
+            </div>
+            <div class="col-sm-3">
+                {!! Form::label('Branch', 'Branch:', ['class' => 'control-label']) !!}
+                {!! Form::select('pBranch', $branch_id,Input::old('pBranch'),['class' => 'form-control','required','title'=>'select  branch, example :: Main Branch']) !!}
+            </div>
+            <div class="col-sm-3">
+                {!! Form::label('FromDate', 'FromDate:', ['class' => 'control-label']) !!}
+                {!! Form::text('pFromDate', date('Y/m/d'), ['class' => 'form-control bs-datepicker-component','required','title'=>'select from date']) !!}
+            </div>
+            <div class="col-sm-3">
+                {!! Form::label('ToDate', 'ToDate:', ['class' => 'control-label']) !!}
+                {!! Form::text('pToDate', date('Y/m/d'), ['class' => 'form-control bs-datepicker-component','required','title'=>'select to date']) !!}
+            </div>
+        </div>
+    </div>
+</div>
+
+<p> &nbsp; </p>
+
+<div class="modal-footer">
+    {!! Form::submit('PDF Report', ['class' => 'btn btn-primary','data-placement'=>'top','data-content'=>'click save changes button for save chart of accounts information']) !!}
+    <a href="{{route('account-reports')}}" class=" btn btn-primary" data-placement="top" data-content="click close button for close this entry form">PDF Report</a>
+    <a href="{{route('account-reports')}}" class=" btn btn-default" data-placement="top" data-content="click close button for close this entry form">Close</a>
+</div>
+
+
+
+<script type="text/javascript" src="{{ URL::asset('assets/admin/js/datepicker.js') }}"></script>
+
+
+
+
+
+
