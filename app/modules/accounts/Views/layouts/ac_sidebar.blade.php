2c3dcc861fa49f7fe1a9c7138064d87c2badfedf
shajjadhossain <shajjadhossain81@gmail.com>
2016-02-18 17:19:49 +0600

    #1 module wise side bar kora hoece

---
+++ app/modules/accounts/Views/layouts/ac_sidebar.blade.php
@@ -0,0 +1,57 @@
+<li class="mm-dropdown">
+    <a href="#"><i class="fa fa-usd"></i><i class="fa fa-usd"></i> <span class="mm-text mmc-dropdown-delay animated fadeIn">General Ledger</span></a>
+    <ul class="mmc-dropdown-delay animated fadeInLeft">
+        <li>
+            <a tabindex="-1" href="{{route('chart-of-accounts')}}"><i class="fa fa-list-alt"></i> <span class="mm-text">Chart of Accounts</span></a>
+        </li>
+        <li>
+            <a tabindex="-1" href="{{route('voucher-head')}}"><i class="fa fa-money"></i> <span class="mm-text">Journal Voucher</span></a>
+        </li>
+        <li>
+            <a tabindex="-1" href="{{route('reverse-voucher')}}"><i class="fa fa-money"></i> <span class="mm-text">Reverse Entry</span></a>
+        </li>
+        <li>
+            <a tabindex="-1" href="{{route('payment-voucher')}}"><i class="fa fa-money"></i> <span class="mm-text">Payment Voucher</span></a>
+        </li>
+        <li>
+            <a tabindex="-1" href="{{route('receipt-voucher')}}"><i class="fa fa-money"></i> <span class="mm-text">Receipt Voucher</span></a>
+        </li>
+
+        <li>
+            <a tabindex="-1" href="{{route('settings')}}"><i class="fa  fa-gear"></i> <span class="mm-text">Settings</span></a>
+        </li>
+        <li>
+            <a tabindex="-1" href="{{route('account-reports')}}"><i class="fa fa-clipboard"></i> <span class="mm-text">Reports</span></a>
+        </li>
+
+        {{--  <li> ------------------------------ </li>
+
+          <li>
+              <a tabindex="-1" href="{{route('default-offset')}}"><span class="mm-text">Default Offset</span></a>
+          </li>
+          <li>
+              <a tabindex="-1" href="{{route('gl-transaction')}}"><span class="mm-text">GL Transaction</span></a>
+          </li>
+          <li>
+              <a tabindex="-1" href="{{route('voucher-history')}}"><span class="mm-text">Voucher History</span></a>
+          </li>--}}
+
+    </ul>
+</li>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
