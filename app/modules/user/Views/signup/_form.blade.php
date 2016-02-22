f0217c15edf6ed4a34e1954971d6631bade31186
Tanin <tanin@edutechsolutionsbd.com>
2016-02-22 18:17:20 +0600

    reset password:back to login page

--- app/modules/user/Views/signup/_form.blade.php
+++ app/modules/user/Views/signup/_form.blade.php
@@ -5,7 +5,7 @@
         <!-- Form -->
 <div class="signup-form">
 
-    {!! Form::open(['route' => 'signup','id'=>'signup-form_id']) !!}
+    {!! Form::open(['route' => 'signup']) !!}
 
         <div class="signup-text">
             <span>Create an account</span>
@@ -23,12 +23,12 @@
             {!! Form::password('password', ['class' => 'form-control input-lg','required','placeholder'=>'Password']) !!}
         </div>
 
-        <div class="form-group" style="margin-top: 20px;margin-bottom: 20px;">
+        {{--<div class="form-group" style="margin-top: 20px;margin-bottom: 20px;">
             <label class="checkbox-inline">
                 {!! Form::checkbox('remember_me', null,['name'=>'signup_confirm','class'=>'px','id'=>'confirm_id']) !!}
                 <span class="lbl">I agree with the <a href="#" target="_blank">Terms and Conditions</a></span>
             </label>
-        </div>
+        </div>--}}
 
         <div class="form-actions">
             <input type="submit" value="SIGN UP" class="signup-btn bg-primary">
