2ff2dc72812c75b0a169a43c4629f099ded1e4f8
Selim Reza <selim@ETSB.com>
2016-02-22 18:07:15 +0600

    layout : sidebar menu

--- app/modules/user/Views/layouts/user_sidebar.blade.php
+++ app/modules/user/Views/layouts/user_sidebar.blade.php
@@ -1,22 +1,21 @@
-<?php  ?>
-<li class="mm-dropdown open mm-dropdown-root ">
+<li class="mm-dropdown">
     <a href="#"><i class="menu-icon fa fa-user-md"></i><span class="mm-text">User</span></a>
     <ul>
-        <li class="active">
+        <li>
             <a tabindex="-1" href="{{route('user-list')}}"><span class="mm-text">User List</span></a>
         </li>
         <li>
             <a tabindex="-1" href="{{route('role')}}"><span class="mm-text">Role</span></a>
         </li>
-        {{--<li>
+        <li>
             <a tabindex="-1" href="{{route('index-role-user')}}"><span class="mm-text">Role User</span></a>
-        </li>--}}
+        </li>
         <li>
             <a tabindex="-1" href="{{route('index-permission')}}"><span class="mm-text">Permission</span></a>
         </li>
-        {{--<li>
+        <li>
             <a tabindex="-1" href="{{route('index-permission-role')}}"><span class="mm-text">Permission Role</span></a>
-        </li>--}}
+        </li>
         <li>
             <a tabindex="-1" href="{{route('create-sign-up')}}"><span class="mm-text">Sign Up</span></a>
         </li>
@@ -28,9 +27,4 @@
         </li>
     </ul>
 </li>
-<script type="text/javascript">
-    $(document).on('click', '.nav-list li', function() {
-        $(".nav-list li").removeClass("active");
-        $(this).addClass("active");
-    });
-</script>
+
