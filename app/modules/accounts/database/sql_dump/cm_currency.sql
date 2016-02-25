9c39e55cef1339b9f4f5291b3fdd73cf320f7658
Tanin <tanin@edutechsolutionsbd.com>
2016-02-25 16:43:38 +0600

    #3 checked 'cm_branch' table exists or not to migrate 'user' table

--- app/modules/accounts/database/sql_dump/cm_currency.sql
+++ app/modules/accounts/database/sql_dump/cm_currency.sql
@@ -3,5 +3,5 @@
 --
 
 INSERT INTO `cm_currency` (`id`, `code`, `title`, `description`, `exchange_rate`, `status`, `created_by`, `updated_by`, `created_at`, `updated_at`) VALUES
-(1, 'BDT', 'Bangladeshi Taka', 'Bangladeshi Taka', 1.00, 'active', NULL, NULL, '2016-01-31 00:00:50', '2016-01-31 00:00:50');
+(1, 'BDT', 'Bangladeshi Taka', 'Bangladeshi Taka', 1.00, 'active', 1, NULL, '2016-01-31 00:00:50', '2016-01-31 00:00:50');
 
