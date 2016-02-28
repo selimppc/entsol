0a7f9ccda14d24b9e880a550e9ffa6c75d85b32c
Nadim <nhsajib316@gmail.com>
2016-02-28 10:29:02 +0600

    Issue #10 : Accounting log filename change in LogFileHelperAcc

--- app/modules/accounts/Helpers/LogFileHelperAcc.php
+++ app/modules/accounts/Helpers/LogFileHelperAcc.php
@@ -13,6 +13,7 @@ class LogFileHelperAcc
     private static function log_file($label_name = null,$file_name = 'accounting_log'){
         $log = new Monolog\Logger($label_name);
         $log->pushHandler(new Monolog\Handler\StreamHandler(app_path().'/modules/accounts/logs/'.$file_name.''.date('Y-m-d').'.log'));
+        umask(0);
         chmod(app_path().'/modules/accounts/logs/'.$file_name.''.date('Y-m-d').'.log', 777);
         return $log;
     }
