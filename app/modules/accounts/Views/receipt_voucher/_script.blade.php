fdb3c875836c8add21e59ed59155b31c83e374c2
shajjadhossain <shajjadhossain81@gmail.com>
2016-03-14 16:30:26 +0600

    #6 receipt voicher single page entry works done

---
+++ app/modules/accounts/Views/receipt_voucher/_script.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: etsb
+ * Date: 3/14/16
+ * Time: 3:25 PM
+ */
---
+++ app/modules/accounts/Views/receipt_voucher/update_script.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: etsb
+ * Date: 3/14/16
+ * Time: 3:58 PM
+ */
