679dd44e9c2d62e7e136d0115a1f3ccfc960e8a9
shajjadhossain <shajjadhossain81@gmail.com>
2016-03-15 15:24:20 +0600

    #6 work on dynamically menu populate in sidebar

--- app/modules/admin/Controllers/AdminController.php
+++ app/modules/admin/Controllers/AdminController.php
@@ -3,10 +3,12 @@
 namespace App\Modules\Admin\Controllers;
 
 use App\Permission;
+use App\RoleUser;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Auth;
+use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
@@ -31,16 +33,20 @@ class AdminController extends Controller
     public function sidebar_menu(){
 
         $user_id = Auth::user()->id;
-        $data = new Permission();
 
-        $data = $data->select('permissions.title');
-        $data = $data->leftJoin('permission_role','permission_role.permission_id','=','permissions.id');
-        $data = $data->leftJoin('role_user','role_user.role_id','=','permission_role.role_id');
-        $data = $data->leftJoin('user','user.id','=','role_user.user_id');
-        $data = $data->where('user.id','=',$user_id);
-        $data = $data->paginate(30);
+        $role_list = RoleUser::where('user_id','=',$user_id)->first();
 
-        print_r($data);exit;
+
+       $permission_route = DB::table('permissions')
+           ->join('permission_role', 'permission_role.permission_id', '=', 'permissions.id')
+           ->join('role', 'role.id', '=', 'permission_role.role_id')
+           ->where('permission_role.role_id', '=', $role_list->role_id)
+           ->select('permissions.route_url')
+           ->paginate(30);
+
+        print_r($permission_route);exit;
+
+        
     }
 
 
