7ac32599311e0641d53d20b25d09dfa6411ae3bc
Tanin <tanin@edutechsolutionsbd.com>
2016-03-16 15:18:50 +0600

    user-login-history table added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,37 +23,40 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(!Session::has('sidebar_menu_user')) {
-            // Get User ID
-            $user_id = 3; //Auth::user()->id;
+        if(Schema::hasTable('role_user'||'menu_panel')){
+            if(!Session::has('sidebar_menu_user')) {
+                // Get User ID
+                $user_id = 3; //Auth::user()->id;
 
-            //get Role(s)
-            $role_list = RoleUser::where('user_id','=',$user_id)
-                ->select('role_user.role_id')
-                ->get()->toArray();
+                //get Role(s)
+                $role_list = RoleUser::where('user_id','=',$user_id)
+                    ->select('role_user.role_id')
+                    ->get()->toArray();
 
 
-            //routes per role(s)
-            $permis_route = Permission::join('permission_role', 'permission_role.permission_id', '=', 'permissions.id')
-                ->whereIn('permission_role.role_id', $role_list)
-                ->select('permissions.route_url')
-                ->get()->toArray();
-            //module route
-            $arr []=[
-                'route_url'=>'#'
-            ];
-            // Merge all routes per ROLE(S) and USER ID
-            $per_routes = array_merge($permis_route,$arr);
+                //routes per role(s)
+                $permis_route = Permission::join('permission_role', 'permission_role.permission_id', '=', 'permissions.id')
+                    ->whereIn('permission_role.role_id', $role_list)
+                    ->select('permissions.route_url')
+                    ->get()->toArray();
+                //module route
+                $arr []=[
+                    'route_url'=>'#'
+                ];
+                // Merge all routes per ROLE(S) and USER ID
+                $per_routes = array_merge($permis_route,$arr);
 
-            //Get Menu Lists by PERMISSION (ROLE+USER+Permission)
-            $tree = MenuPanel::whereIn('menu_panel.route',$per_routes)->orderBy('menu_order', 'ASC')->get()->toArray();
-            $parent = 0;
+                //Get Menu Lists by PERMISSION (ROLE+USER+Permission)
+                $tree = MenuPanel::whereIn('menu_panel.route',$per_routes)->orderBy('menu_order', 'ASC')->get()->toArray();
+                $parent = 0;
 
-            $result = MenuItems::menu_tree($tree, $parent);
-            #print_r($result[1]['sub-menu'] [4]['sub-menu'] );exit;
-            // put the menu items in session
-            Session::put('sidebar_menu_user', $result);
+                $result = MenuItems::menu_tree($tree, $parent);
+                #print_r($result[1]['sub-menu'] [4]['sub-menu'] );exit;
+                // put the menu items in session
+                Session::put('sidebar_menu_user', $result);
+            }
         }
+
     }
 
     /**
