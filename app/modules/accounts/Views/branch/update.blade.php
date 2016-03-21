f983a11e4e2a993c3e36dcd5a60df9f512f5db1f
Tanin <tanin@edutechsolutionsbd.com>
2016-03-21 13:47:30 +0600

    #1 exchange rate according to currency in branch

--- app/modules/accounts/Views/branch/update.blade.php
+++ app/modules/accounts/Views/branch/update.blade.php
@@ -9,7 +9,105 @@
 <div class="modal-body">
     @section('content_update')
     {!! Form::model($data, ['method' => 'PATCH', 'route'=> ['update-branch', $data->id]]) !!}
-    @include('accounts::branch._form')
+      {{--@include('accounts::branch._form')--}}
+        <div class="form-group no-margin-hr panel-padding-h no-padding-t no-border-t">
+            <div class="row">
+                <div class="col-sm-6">
+                    {!! Form::label('code', 'Code:', ['class' => 'control-label']) !!}
+                    <small class="required">(Required)</small>
+                    {!! Form::text('code', Input::old('code'), ['id'=>'code', 'class' => 'form-control', 'style'=>'text-transform:uppercase','required','autofocus','title'=>'enter branch code, example :: main']) !!}
+                </div>
+                <div class="col-sm-6">
+                    {!! Form::label('title', 'Title:', ['class' => 'control-label']) !!}
+                    <small class="required">(Required)</small>
+                    {!! Form::text('title', Input::old('title'), ['id'=>'title', 'class' => 'form-control','required','required', 'style'=>'text-transform:capitalize','required','title'=>'enter branch title, example :: Main Branch']) !!}
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group no-margin-hr panel-padding-h no-padding-t no-border-t">
+            <div class="row">
+                <div class="col-sm-12">
+                    {!! Form::label('description', 'Description:', ['class' => 'control-label']) !!}
+                    {!! Form::textarea('description', Input::old('description'), ['id'=>'description', 'class' => 'form-control','size' => '12x3','title'=>'enter descriptions about branch code']) !!}
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group no-margin-hr panel-padding-h no-padding-t no-border-t">
+            <div class="row">
+                <div class="col-sm-6">
+                    {!! Form::label('currency_id', 'Currency:', ['class' => 'control-label']) !!}
+                    <small class="required">(Required)</small>
+                    @if(count($currency_id)>0)
+                        {!! Form::select('currency_id', $currency_id,Input::old('currency_id'),['id'=>'curr_id','class' => 'form-control','required','title'=>'select branch currency, example :: Bangladeshi Taka','onclick'=>'myFunction();']) !!}
+                    @else
+                        {!! Form::text('currency_id', 'No Currency ID available',['id'=>'currency_id','class' => 'form-control','required','disabled']) !!}
+                    @endif
+                </div>
+                <div class="col-sm-6">
+                    {!! Form::label('exchange_rate', 'Exchange Rate:', ['class' => 'control-label']) !!}
+                    <small class="required">(Required)</small>
+                    {!! Form::input('number', 'exchange_rate', Input::old('exchange_rate'), ['id'=>'change-exchange_rate', 'class' => 'form-control', 'step'=>'any','required','title'=>'enter exchange rate for selected currency']) !!}
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group no-margin-hr panel-padding-h no-padding-t no-border-t">
+            <div class="row">
+                <div class="col-sm-12">
+                    {!! Form::label('billing_address', 'Billing Address:', ['class' => 'control-label']) !!}
+                    {!! Form::textarea('billing_address', Input::old('billing_address'), ['id'=>'billing_address', 'class' => 'form-control','size' => '12x3','title'=>'enter billing address of branch']) !!}
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group no-margin-hr panel-padding-h no-padding-t no-border-t">
+            <div class="row">
+                <div class="col-sm-6">
+                    {!! Form::label('contact_person', 'Contact Person:', ['class' => 'control-label']) !!}
+                    {!! Form::text('contact_person', Input::old('contact_person'), ['id'=>'contact_person', 'class' => 'form-control','required','title'=>'enter contact person of branch']) !!}
+                </div>
+                <div class="col-sm-6">
+                    {!! Form::label('phone', 'Phone:', ['class' => 'control-label']) !!}
+                    {!! Form::input('number','phone', Input::old('phone'), ['id'=>'phone', 'class' => 'form-control','required','title'=>'enter phone number of branch']) !!}
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group no-margin-hr panel-padding-h no-padding-t no-border-t">
+            <div class="row">
+                <div class="col-sm-6">
+                    {!! Form::label('mobile', 'Mobile:', ['class' => 'control-label']) !!}
+                    {!! Form::input('number','mobile', Input::old('mobile'), ['id'=>'mobile', 'class' => 'form-control','title'=>'enter mobile number of branch']) !!}
+                </div>
+                <div class="col-sm-6">
+                    {!! Form::label('fax', 'Fax:', ['class' => 'control-label']) !!}
+                    {!! Form::text('fax', Input::old('fax'), ['id'=>'fax', 'class' => 'form-control','title'=>'enter fax number of branch']) !!}
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group no-margin-hr panel-padding-h no-padding-t no-border-t">
+            <div class="row">
+                <div class="col-sm-6">
+                    {!! Form::label('email', 'Email:', ['class' => 'control-label']) !!}
+                    {!! Form::email('email', Input::old('email'), ['id'=>'email', 'class' => 'form-control','required','title'=>'enter email address of branch']) !!}
+                </div>
+                <div class="col-sm-6">
+                    {!! Form::label('status', 'Status:', ['class' => 'control-label']) !!}
+                    <small class="required">(Required)</small>
+                    {!! Form::select('status', array('active'=>'Active','inactive'=>'Inactive','cancel'=>'Cancel'),Input::old('status'),['class' => 'form-control','required','title'=>'select status of branch']) !!}
+                </div>
+            </div>
+        </div>
+
+        <p> &nbsp; </p>
+
+        <div class="form-margin-btn">
+            {!! Form::submit('Save changes', ['class' => 'btn btn-primary','data-placement'=>'top','data-content'=>'click save changes button for save branch information']) !!}
+            <a href="{{route('branch')}}" class=" btn btn-default" data-placement="top" data-content="click close button for close this entry form">Close</a>
+        </div>
     {!! Form::close() !!}
 </div>
 
@@ -45,4 +143,19 @@
         d.tooltip();
         return d;
     });
+</script>
+
+<script>
+    function myFunction() {
+
+        var curr_id = $('select[id=curr_id]').val();
+        $.ajax({
+            url: "{{Route('exchange-rate')}}",
+            type: 'POST',
+            data: {_token: '{!! csrf_token() !!}',currency_id: curr_id },
+            success: function(data){
+                $('#change-exchange_rate').val(data);
+            }
+        });
+    }
 </script>
