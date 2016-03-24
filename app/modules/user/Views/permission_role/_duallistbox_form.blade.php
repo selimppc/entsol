9902afaedaf05f255c59f3f1ecd075968ce753c9
Tanin <tanin@edutechsolutionsbd.com>
2016-03-24 15:25:14 +0600

    permission role

--- app/modules/user/Views/permission_role/_duallistbox_form.blade.php
+++ app/modules/user/Views/permission_role/_duallistbox_form.blade.php
@@ -1,30 +1,63 @@
+<script type="text/javascript" src="{{ URL::asset('assets/admin/js/jquery.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('assets/admin/js/multiselect.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('assets/admin/js/multiselect.min.js') }}"></script>
 
-{{--<div class="modal-body">
-    {!! Form::model($data, ['method' => 'PATCH', 'route'=> ['post-permission']]) !!}--}}
 
-    <div class="form-group no-margin-hr panel-padding-h no-padding-t no-border-t">
-        <div class="row">
-            <div class="form-group col-sm-12">
-                {!! Form::label('permission_id', 'Select Permission :', ['class' => 'control-label']) !!}
-                {!! Form::select('permission_id',$exists_permission,Input::old('permission_id'),['id' => 'route-list','class'=>'permission_list','multiple' => 'multiple']) !!}
-            </div>
-        </div>
+<div class="row">
+    <div class="col-sm-5">
+        <select name="permission_id" id="optgroup" class="form-control" size="20" multiple="multiple">
+            @foreach($not_exists_permission as $not_ex_per)
+                   <option value="">{{$not_ex_per}}</option>
+            @endforeach
+        </select>
     </div>
 
-    <div class="form-margin-btn">
-        {!! Form::submit('Save changes', ['class' => 'btn btn-primary','data-placement'=>'top','data-content'=>'click save changes button for save information']) !!}
-        <a href="{{route('index-permission-role')}}" class=" btn btn-default" data-placement="top" data-content="click close button for close this entry form">Close</a>
+    <div class="col-sm-2">
+        <button type="button" id="optgroup_rightAll" class="btn btn-block"><i class="glyphicon glyphicon-forward"></i></button>
+        <button type="button" id="optgroup_rightSelected" class="btn btn-block"><i class="glyphicon glyphicon-chevron-right"></i></button>
+        <button type="button" id="optgroup_leftSelected" class="btn btn-block"><i class="glyphicon glyphicon-chevron-left"></i></button>
+        <button type="button" id="optgroup_leftAll" class="btn btn-block"><i class="glyphicon glyphicon-backward"></i></button>
     </div>
-    {{--{!! Form::close() !!}
+
+    <div class="form-group col-sm-5">
+        {{--{!! Form::label('to', 'Select Permission :', ['class' => 'control-label']) !!}
+        {!! Form::select('to',$exists_permission,Input::old('permission_id'),['class' => 'route-list','id'=>'optgroup_to','multiple' => 'multiple','size'=>'']) !!}--}}
+
+        <select name="permissions_id" id="optgroup_to" class="form-control" size="20" multiple="multiple">
+            @foreach($exists_permission as $ex_per)
+                <option value="">{{$ex_per}}</option>
+            @endforeach
+        </select>
+    </div>
+
 </div>
---}}
 
+
+<div class="form-margin-btn">
+    {!! Form::submit('Save changes', ['class' => 'btn btn-primary','data-placement'=>'top','data-content'=>'click save changes button for save information']) !!}
+    <a href="{{route('index-permission-role')}}" class=" btn btn-default" data-placement="top" data-content="click close button for close this entry form">Close</a>
+</div>
 
 <p> &nbsp; </p>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-<script type="text/javascript" src="{{ URL::asset('assets/admin/js/jquery.bootstrap-duallistbox.js') }}"></script>
+
 
 <script type="text/javascript">
-    $(".permission_list").bootstrapDualListbox();
+    jQuery(document).ready(function($) {
+        $("#optgroup").multiselect({
+            search: {
+                left: '<input type="text" name="q" class="form-control" placeholder="Search..." />',
+                right: '<input type="text" name="q" class="form-control" placeholder="Search..." />',
+            }
+        });
+    });
 </script>
+
+
+
+
+
+
+
+
+
 
