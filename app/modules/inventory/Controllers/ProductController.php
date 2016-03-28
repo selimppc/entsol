3476d4e4d4eb41ab65958d2feceadbb0688db8f7
Tanin <tanin@edutechsolutionsbd.com>
2016-03-28 15:26:46 +0600

    #12 basic function declaration  added in all Controllers

--- app/modules/inventory/Controllers/ProductController.php
+++ app/modules/inventory/Controllers/ProductController.php
@@ -14,4 +14,75 @@ use App\Http\Controllers\Controller;
 class ProductController extends Controller
 {
 
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function index(){
+
+
+    }
+
+    public function search(){
+
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(){
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id){
+
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id){
+
+
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update($id){
+
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function delete($id){
+
+
+    }
 }
